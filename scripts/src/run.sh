b1bcc7b4023bd4143742e9a2d788c0024f7f2432
Remo <pagaiaonboat@gmail.com>
2021-04-01 19:09:12 +0200

    fixed scripts to fetch previous month

--- scripts/src/run.sh
+++ scripts/src/run.sh
@@ -3,5 +3,5 @@
 # execute script each first day of the month
 #0 3 1 * * /home/user/path/to/script.sh
 
-cd /home/user/path/to/nodejs/script/directory/
-/home/user/.nvm/versions/node/v12.17.0/bin/node /home/user/index.js
+# cd /home/user/path/to/nodejs/script/directory/
+node src/run.js
